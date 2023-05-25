bb4e552e6e1656d58119aedf4db7a3fa3a3f5de3
amtune <allisontune@gmail.com>
2023-05-25 19:43:21 -0400

    created index.js

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const User = require('./User');
-const Project = require('./Stories');
+const Stories = require('./Stories');
 
-User.hasMany(Project, {
+User.hasMany(Stories, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Stories.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+module.exports = { User, Stories };
