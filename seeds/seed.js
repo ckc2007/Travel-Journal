19e6d039a273c85a0e1ddf15f97fa148fc28cdf8
Seth Sun <sithsun@gmail.com>
2023-05-26 16:56:45 -0400

    seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,44 +1,3 @@
-// const sequelize = require('../config/connection');
-// const { User, Stories, Comments } = require('../models');
-
-// const userData = require('./userData.json');
-// const storyData = require('./storyData.json');
-// const commentData = require('./commentData.json');
-
-// const seedDatabase = async () => {
-//   await sequelize.sync({ force: true });
-
-//   const users = await User.bulkCreate(userData, {
-//     individualHooks: true,
-//     returning: true,
-//   });
-
-//   const stories = await Stories.bulkCreate(
-//     storyData.map((story) => ({
-//       ...story,
-//       user_id: users[Math.floor(Math.random() * users.length)].id,
-//     })),
-//     {
-//       returning: true,
-//     }
-//   );
-
-//   for (const comment of commentData) {
-//     const randomUser = users[Math.floor(Math.random() * users.length)];
-//     const randomStory = stories[Math.floor(Math.random() * stories.length)];
-
-//     await Comments.create({
-//       ...comment,
-//       user_id: randomUser.id,
-//       story_id: randomStory.id,
-//     });
-//   }
-
-//   process.exit(0);
-// };
-
-// seedDatabase();
-
 const sequelize = require('../config/connection');
 const { User, Stories, Comments } = require('../models');
 
@@ -66,8 +25,8 @@ const seedDatabase = async () => {
 
   await Comments.bulkCreate(
     commentData.map((comment) => {
-      const randomUser = users[Math.floor(Math.random() * users.length)];
-      const randomStory = stories[Math.floor(Math.random() * stories.length)];
+      const randomUser = users[Math.floor(Math.random() * users.length)].id;
+      const randomStory = stories[Math.floor(Math.random() * stories.length)].id;
       return {
         ...comment,
         user_id: randomUser.id,
