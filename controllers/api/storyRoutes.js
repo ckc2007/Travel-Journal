4c7d504613244a913d83ee79c2f2716651abea56
Seth Sun <sithsun@gmail.com>
2023-05-25 21:59:28 -0400

    debugging broken array

--- controllers/api/storyRoutes.js
+++ controllers/api/storyRoutes.js
@@ -8,7 +8,7 @@ router.post('/', withAuth, async (req, res) => {
       ...req.body,
       user_id: req.session.user_id,
     });
-
+    console.log(newStory)
     res.status(200).json(newStory);
   } catch (err) {
     res.status(400).json(err);
