d3118244792b0808b86ef883746bd3ee0a0c468d
Seth Sun <sithsun@gmail.com>
2023-05-27 08:03:18 -0400

    Fixed error where story_id was showing null in comments table. Creating comment properly assign story_id. Must be signed in.

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,12 +1,36 @@
+// const router = require('express').Router();
+// const { Comments } = require('../../models');
+// const withAuth = require('../../utils/auth');
+
+// router.post('/', async (req, res) => {
+//   try {
+//     const { story_id, comments } = req.body;
+
+//     // Retrieve the authenticated user ID
+//     const userId = req.session.user_id;
+
+//     const commentData = await Comments.create({
+//       comments,
+//       user_id: userId,
+//       story_id,
+//     });
+
+//     res.status(200).json(commentData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
+// module.exports = router;
+
+
 const router = require('express').Router();
 const { Comments } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   try {
-    const { story_id, comments } = req.body;
-
-    // Retrieve the authenticated user ID
+    const { comments, story_id } = req.body;
     const userId = req.session.user_id;
 
     const commentData = await Comments.create({
--- public/js/story.js
+++ public/js/story.js
@@ -1,22 +1,65 @@
+// const newCommentHandler = async (event) => {
+//   event.preventDefault();
+
+//  let text = document.querySelector('#comment-textarea').value.trim();
+
+//   if (text) {
+//     try {
+//       const response = await fetch(`/api/comments`, {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ story_id: storyId, comments: text }),
+//       });
+
+//       if (response.ok) {
+//         const commentData = await response.json();
+//         // Handle the successful creation of the comment
+//         console.log('Comment created:', commentData);
+//         document.querySelector('#comment-textarea').value = '';
+//       } else {
+//         // Handle the error case
+//         console.error('Failed to create comment:', response.status);
+//       }
+//     } catch (error) {
+//       // Handle any network or other errors
+//       console.error('Error creating comment:', error);
+//     }
+//   }
+// };
+
+// const userComments = (commentData) => {
+//   const comments = JSON.stringify(commentData);
+//   return comments;
+// };
+
+// document
+//   .querySelector('#comment-submit')
+//   .addEventListener('click', newCommentHandler);
+
+
 const newCommentHandler = async (event) => {
   event.preventDefault();
-
+  
   const text = document.querySelector('#comment-textarea').value.trim();
-
-  if (text) {
+  const storyId = window.location.pathname.split('/').pop();
+  
+  if (text && storyId) {
     try {
-      const response = await fetch(`/api/comments`, {
+      const response = await fetch('/api/comments', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ story_id: storyId, comments: text }),
-      });
-
+          },
+        body: JSON.stringify({ comments: text, story_id: storyId }),
+       });
+  
       if (response.ok) {
         const commentData = await response.json();
         // Handle the successful creation of the comment
         console.log('Comment created:', commentData);
+        // Perform any additional UI updates if needed
       } else {
         // Handle the error case
         console.error('Failed to create comment:', response.status);
@@ -27,7 +70,7 @@ const newCommentHandler = async (event) => {
     }
   }
 };
-
+  
 document
   .querySelector('#comment-submit')
   .addEventListener('click', newCommentHandler);
