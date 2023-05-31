cf8d1b3aacc322073d679c263df4307292acc1a1
ckc2007 <ckc2007@gmail.com>
2023-05-31 19:33:41 -0400

    brought in upload from utils

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,20 +1,19 @@
 //IMPORT ALL NECESSARY FILES AND FOLDERS
-const router = require('express').Router();
-const { Story, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
-
-
+const router = require("express").Router();
+const { Story, User, Comment } = require("../models");
+const upload = require("../utils/upload");
+const withAuth = require("../utils/auth");
 
 //----HOMEPAGE GET REQUEST----
 // Homepage, using Story model with User attributes, renders it to homepage!
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // Get all projects and JOIN with user data
     const storyData = await Story.findAll({
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ],
     });
@@ -23,9 +22,9 @@ router.get('/', async (req, res) => {
     const stories = storyData.map((story) => story.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      stories, 
-      logged_in: req.session.logged_in 
+    res.render("homepage", {
+      stories,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -33,17 +32,15 @@ router.get('/', async (req, res) => {
 });
 //----END of GET REQUEST---
 
-
-
 //----STORY WITH COMMENTS GET REQUEST----
 // Story mode with User attribute to display story and associated comments
-router.get('/stories/:id', async (req, res) => {
+router.get("/stories/:id", async (req, res) => {
   try {
     const storyData = await Story.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ],
     });
@@ -59,16 +56,18 @@ router.get('/stories/:id', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ],
     });
 
     //store commentData collected into comments and serialize it
-    const comments = commentsData.map((comment) => comment.get({ plain: true }));
+    const comments = commentsData.map((comment) =>
+      comment.get({ plain: true })
+    );
 
     //render both story and comments and check if user is logged_in
-    res.render('story', {
+    res.render("story", {
       ...story,
       comments,
       logged_in: req.session.logged_in,
@@ -79,21 +78,20 @@ router.get('/stories/:id', async (req, res) => {
 });
 //----END of GET REQUEST---
 
-
 //----PROFILE GET REQUEST----
 // User model with Story model attributes and checks to see if they're logged in first
-router.get('/profile', withAuth, async (req, res) => {
+router.get("/profile", withAuth, async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ["password"] },
       include: [{ model: Story }],
     });
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render("profile", {
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -101,30 +99,20 @@ router.get('/profile', withAuth, async (req, res) => {
 });
 //----END of GET REQUEST---
 
-
-
-
-
 //NEW GET REQUEST CODE GOES HERE
-
-
-
-
-
 
 //----LOGIN GET REQUEST----
 // Renders login page for not-logged-in, and redirects to /profile for those logged-in
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect("/profile");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 //----END of GET REQUEST---
-
 
 //EXPORT THE ROUTER FOR OTHER FILES AND FOLDERS TO USE
 module.exports = router;
