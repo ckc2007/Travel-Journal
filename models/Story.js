0d5026924d6d35106f01669a95d6ef94a183917a
ckc2007 <ckc2007@gmail.com>
2023-05-31 20:15:00 -0400

    image key debug continues

--- models/Story.js
+++ models/Story.js
@@ -1,6 +1,5 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
-const multer = require("multer");
 
 class Story extends Model {}
 
@@ -50,18 +49,4 @@ Story.init(
   }
 );
 
-// module.exports = Story;
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/images"); // Specify the directory where uploaded images will be stored
-  },
-  filename: function (req, file, cb) {
-    // Generate a unique filename for the uploaded file
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage }).array("image", 5);
-
-module.exports = { Stories, upload };
+module.exports = Story;
