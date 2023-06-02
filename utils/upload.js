2012290bdc3c122be883d111ee4ef78b36aa001b
ckc2007 <ckc2007@gmail.com>
2023-06-01 21:52:11 -0400

    changed to single image handler

--- utils/upload.js
+++ utils/upload.js
@@ -11,6 +11,6 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage }).single("image");
+const upload = multer({ storage: storage });
 
 module.exports = upload;
