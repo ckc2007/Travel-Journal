f3dff66ac895cbf9e114e21682a857058f4e939a
Seth Sun <sithsun@gmail.com>
2023-06-05 19:36:29 -0400

    Added function for searching in trip planner. Working on code.

--- public/js/tripplanner.js
+++ public/js/tripplanner.js
@@ -1,36 +1,35 @@
-//Search Database for Trip Planner
-const searchTripPlanner= async (event) => {
-    event.preventDefault();
-    
-    const searchForm = document.querySelector('#search-form');
-    const searchTerm = document.querySelector('#search-term').value.trim();
-    
-    if (text && storyId) {
-      try {
-        const response = await fetch(`/api/trips/search?term=${searchTerm}`, {
-          method: 'GET',
-          body: JSON.stringify({ text, storyId }),
-          headers: {
-            'Content-Type': 'application/json',
-            },
-         });
-    
-        if (response.ok) {
-          const commentData = await response.json();
-          console.log('Comment created:', commentData);
-          document.location.reload();
-  
-        } else {
-          console.error('Failed to create comment:', response.status);
-          document.location.href = document.location.origin + '/login';
-        }
-      } catch (error) {
-        console.error('Error creating comment:', error);
-        document.location.href = document.location.origin + '/login';
-        }
-    }
-  };
+// Search Trips Database
+const searchTripPlanner = async (event) => {
+  event.preventDefault();
 
-  document
-  .querySelector('#comment-submit')
-  .addEventListener('click', searchTripPlanner);
+  const searchTerm = document.querySelector("#search-term").value.trim();
+
+  if (searchTerm) {
+    try {
+      const response = await fetch(`/api/trips/search?term=${searchTerm}`);
+      const tripData = await response.json();
+      console.log("Search results:", tripData);
+
+      // Clear previous search results
+      const searchResults = document.getElementById("search-results");
+      searchResults.innerHTML = "";
+
+      // Display the search results
+      tripData.forEach((trip) => {
+        const tripElement = document.createElement("div");
+        tripElement.innerHTML = `
+                <h3>${trip.tripname}</h3>
+                <p>${trip.description}</p>
+                <p>Budget: ${trip.budget}</p>
+              `;
+        searchResults.appendChild(tripElement);
+      });
+    } catch (error) {
+      console.error("Error searching trips:", error);
+    }
+  }
+};
+
+document
+  .querySelector("#search-form")
+  .addEventListener("submit", searchTripPlanner);
